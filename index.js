de6f676e54a8435b9516d0ba4adad8561c4c9986
patricv <patricv13@gmail.com>
2022-03-03 19:42:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 const PORT = process.env.PORT || 80
 const cors = require('cors')
 const users = require('./routes/users')
+const notFound = require('./middleware/errors/notFound')
 
 app.use(cors())
 app.use(express.json())
 app.use('/users', users)
+app.use(notFound)
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`)
