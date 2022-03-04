835b24cfb0bd5d709616b4111dd219129697e521
patricv <patricv13@gmail.com>
2022-03-04 09:21:58 +0100

    error conection mongo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('./settings/mongo')
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 80
--- settings/mongo.js
+++ settings/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
-const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.le5dx.mongodb.net/${dbUser}?retryWrites=true&w=majority`
-
+const db = process.env.DB
+const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.le5dx.mongodb.net/${db}?retryWrites=true&w=majority`
 mongoose.connect(connectionString).then(() => {
   console.log('Database connected')
 }).catch((error) => {
