6ab041d4558fe3863898264f81e45ac5b43e1c1f
patricv <patricv13@gmail.com>
2022-03-06 13:57:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
 require('./settings/mongo')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 80
 const cors = require('cors')
 const users = require('./routes/Users')
+const tecnoturist = require('./routes/Tecnoturist')
 const notFound = require('./middleware/errors/notFound')
 const errorHandler = require('./middleware/errors/handleError')
 
 app.use(cors())
 app.use(express.json())
 app.use('/users', users)
+app.use('/tecnoturist', tecnoturist)
 app.use(notFound)
 app.use(errorHandler)
 
